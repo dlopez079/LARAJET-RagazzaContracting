d5e9540cfcf01d65b07a8f28e18efd2eab834d2d
David Lopez <dlopez@mattmen.com>
2022-02-24 11:42:38 -0500

    Created About Section

--- bootstrap/cache/livewire-components.php
+++ bootstrap/cache/livewire-components.php
@@ -1,4 +1,5 @@
 <?php return array (
+  'about-section' => 'App\\Http\\Livewire\\AboutSection',
   'hero-section' => 'App\\Http\\Livewire\\HeroSection',
   'services-section' => 'App\\Http\\Livewire\\ServicesSection',
 );
