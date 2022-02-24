2a7a4de56d78b4804d7ec8ab08775f78647c5bda
David Lopez <dlopez@mattmen.com>
2022-02-24 11:43:24 -0500

    Created Contact Section

--- bootstrap/cache/livewire-components.php
+++ bootstrap/cache/livewire-components.php
@@ -1,5 +1,6 @@
 <?php return array (
   'about-section' => 'App\\Http\\Livewire\\AboutSection',
+  'contact-section' => 'App\\Http\\Livewire\\ContactSection',
   'hero-section' => 'App\\Http\\Livewire\\HeroSection',
   'services-section' => 'App\\Http\\Livewire\\ServicesSection',
 );
