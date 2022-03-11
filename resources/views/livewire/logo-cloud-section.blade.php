c744c6b7d06cbff0e1a9fe76b21bbba1c39241e5
David Lopez <dlopez@mattmen.com>
2022-03-11 13:29:52 -0500

    Logo-Cloud: Partner Logo Addition. I added DASNY Logo to section. I added NYCHA Logo to section. I added NYCSCA Logo to section. I removed the logos the template was using. The logos are src'ed with external link. I wrote out the entire name of the company for alternate attribute.

--- resources/views/livewire/logo-cloud-section.blade.php
+++ resources/views/livewire/logo-cloud-section.blade.php
@@ -1,25 +1,33 @@
 <section id="logo-cloud" class="bg-red-700">
     <div class="max-w-7xl mx-auto py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
-      <h2 class="text-3xl font-extrabold text-white">The world's most innovative companies use Workflow</h2>
+
+      {{-- Header Div --}}
+      <h2 class="text-3xl font-extrabold text-white">New York City Partners</h2>
+
+      {{-- Logo Div --}}
       <div class="flow-root mt-8 lg:mt-10">
+
+        {{-- Flex Grid --}}
         <div class="-mt-4 -ml-8 flex flex-wrap justify-between lg:-ml-4">
+
+          {{-- Partner Logo #1: DASNY - Dormitory Authority iof State of New York --}}
           <div class="mt-4 ml-8 flex flex-grow flex-shrink-0 lg:flex-grow-0 lg:ml-4">
-            <img class="h-12" src="https://tailwindui.com/img/logos/tuple-logo-indigo-300.svg" alt="Tuple">
+            <img class="h-16" src="https://static-assets.ny.gov/sites/all/themes/ny_gov/images/nygov-logo.png" alt="Dormitory Authority iof State of New York">
           </div>
+
+          {{-- Partner Logo #2: NYCHA - New York City Housing Authority --}}
           <div class="mt-4 ml-8 flex flex-grow flex-shrink-0 lg:flex-grow-0 lg:ml-4">
-            <img class="h-12" src="https://tailwindui.com/img/logos/mirage-logo-indigo-300.svg" alt="Mirage">
+            <img class="h-16 bg-white" src="https://www1.nyc.gov/assets/nycha/images/content/header/logo.png" alt="New York City Housing Authority">
           </div>
+
+          {{-- Partner Logo #3: NYCSCA - New York City School Construction Authority --}}
           <div class="mt-4 ml-8 flex flex-grow flex-shrink-0 lg:flex-grow-0 lg:ml-4">
-            <img class="h-12" src="https://tailwindui.com/img/logos/statickit-logo-indigo-300.svg" alt="StaticKit">
+            <img class="h-16" src="http://www.nycsca.org/Portals/_default/Skins/SCA_SKIN/Images/SCA_logo_horiz_white.png" alt="New York City School Construction Authority">
           </div>
-          <div class="mt-4 ml-8 flex flex-grow flex-shrink-0 lg:flex-grow-0 lg:ml-4">
-            <img class="h-12" src="https://tailwindui.com/img/logos/transistor-logo-indigo-300.svg" alt="Transistor">
-          </div>
-          <div class="mt-4 ml-8 flex flex-grow flex-shrink-0 lg:flex-grow-0 lg:ml-4">
-            <img class="h-12" src="https://tailwindui.com/img/logos/workcation-logo-indigo-300.svg" alt="Workcation">
-          </div>
+
         </div>
       </div>
+
     </div>
 </section>
   
